1a462620f8fd4c6a9e02f90014942ffd3a75f754
APARNA PARASHAR <145586377+aparnaparashar@users.noreply.github.com>
2025-11-29 22:44:30 +0530

    change ml_models directory

--- ml_models/preprocessing/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    PROJECT_DIR = os.path.abspath(os.path.join(BASE_DIR, "../../"))
-
-    input_file = os.path.join(PROJECT_DIR, "data/raw/traffic_data.csv")
-    output_file = os.path.join(PROJECT_DIR, "data/processed/traffic_data_processed.csv")
